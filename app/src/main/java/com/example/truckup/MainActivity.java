c8e903385b544392427b113fbbd1cc22b638e642
dan_jn <danjan2906@gmail.com>
2024-02-10 19:09:14 +0400

    home activity with fragments

--- app/src/main/java/com/example/truckup/MainActivity.java
+++ app/src/main/java/com/example/truckup/MainActivity.java
@@ -1,21 +1,28 @@
 package com.example.truckup;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationBarView;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
 public class MainActivity extends AppCompatActivity {
-    FirebaseAuth auth;
-    Button button;
-    TextView textView;
-    FirebaseUser user;
+
+    BottomNavigationView bottomNavigationView;
+    HomeFragment homeFragment = new HomeFragment();
+    ProfileFragment profileFragment = new ProfileFragment();
+    MapFragment mapFragment = new MapFragment();
+    TrucksFragment trucksFragment = new TrucksFragment();
+
 
 
 
@@ -23,27 +30,30 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        auth = FirebaseAuth.getInstance();
-        button = findViewById(R.id.logout);
-        textView = findViewById(R.id.user_details);
-        user = auth.getCurrentUser();
-        if(user == null){
-            Intent intent = new Intent(getApplicationContext(), Login.class);
-            startActivity(intent);
-            finish();
-        }
-        else{
-            textView.setText(user.getEmail());
-        }
 
-        button.setOnClickListener(new View.OnClickListener() {
+        bottomNavigationView = findViewById(R.id.bottom_navigation);
+        getSupportFragmentManager().beginTransaction().replace(R.id.container, homeFragment).commit();
+        bottomNavigationView.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
             @Override
-            public void onClick(View view) {
-                FirebaseAuth.getInstance().signOut();
-                Intent intent = new Intent(getApplicationContext(), Login.class);
-                startActivity(intent);
-                finish();
+            public boolean onNavigationItemSelected( MenuItem item) {
+                int id = item.getItemId();
+                if (id == R.id.home) {
+                    getSupportFragmentManager().beginTransaction().replace(R.id.container, homeFragment).commit();
+                    return true;
+                } else if (id == R.id.trucks) {
+                    getSupportFragmentManager().beginTransaction().replace(R.id.container, trucksFragment).commit();
+                    return true;
+                } else if (id == R.id.map) {
+                    getSupportFragmentManager().beginTransaction().replace(R.id.container, mapFragment).commit();
+                    return true;
+                } else if (id == R.id.profile) {
+                    getSupportFragmentManager().beginTransaction().replace(R.id.container, profileFragment).commit();
+                    return true;
+                }
+                return false;
             }
         });
+
+
     }
 }
---
+++ app/src/main/java/com/example/truckup/ProfileFragment.java
@@ -0,0 +1,57 @@
+package com.example.truckup;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+
+public class ProfileFragment extends Fragment {
+
+    FirebaseAuth auth;
+    Button button;
+    TextView textView;
+    FirebaseUser user;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_profile, container, false);
+
+        auth = FirebaseAuth.getInstance();
+        button = view.findViewById(R.id.logout); // Corrected here
+        textView = view.findViewById(R.id.user_details); // Corrected here
+        user = auth.getCurrentUser();
+        if(user == null){
+            Intent intent = new Intent(getActivity().getApplicationContext(), Login.class);
+            startActivity(intent);
+            getActivity().finish();
+        }
+        else{
+            textView.setText(user.getEmail());
+        }
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                FirebaseAuth.getInstance().signOut();
+                Intent intent = new Intent(getActivity().getApplicationContext(), Login.class);
+                startActivity(intent);
+                getActivity().finish();
+            }
+        });
+
+        return view;
+    }
+    }
+
