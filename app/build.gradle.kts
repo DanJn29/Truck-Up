d1b65ff7becec53a07ca80153bca57fcf2c3f5f3
dan_jn <danjan2906@gmail.com>
2024-05-18 11:36:21 +0400

    open map added (didn't have the google maps api)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -56,6 +56,8 @@ dependencies {
     implementation ("com.google.android.gms:play-services-maps:18.2.0")
     implementation ("com.google.android.gms:play-services-location:21.2.0")
     implementation ("com.google.android.libraries.places:places:3.4.0")
+    implementation ("org.osmdroid:osmdroid-android:6.1.10")
+    implementation ("com.android.volley:volley:1.2.1")
 
     annotationProcessor ("com.github.bumptech.glide:compiler:4.12.0")
 
--- app/src/main/java/com/example/truckup/MapFragment.java
+++ app/src/main/java/com/example/truckup/MapFragment.java
@@ -3,6 +3,7 @@ package com.example.truckup;
 import android.Manifest;
 import android.content.pm.PackageManager;
 import android.location.Location;
+import android.net.Uri;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.LayoutInflater;
@@ -10,118 +11,176 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Toast;
 
-import androidx.annotation.NonNull;
 import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
 import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationCallback;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationResult;
 import com.google.android.gms.location.LocationServices;
-import com.google.android.gms.maps.CameraUpdateFactory;
-import com.google.android.gms.maps.GoogleMap;
-import com.google.android.gms.maps.OnMapReadyCallback;
-import com.google.android.gms.maps.SupportMapFragment;
-import com.google.android.gms.maps.model.LatLng;
-import com.google.android.gms.maps.model.MarkerOptions;
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
-import com.google.android.libraries.places.api.Places;
-import com.google.android.libraries.places.api.model.Place;
-import com.google.android.libraries.places.api.model.PlaceLikelihood;
-import com.google.android.libraries.places.api.net.FindCurrentPlaceRequest;
-import com.google.android.libraries.places.api.net.FindCurrentPlaceResponse;
-import com.google.android.libraries.places.api.net.PlacesClient;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.Objects;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.osmdroid.config.Configuration;
+import org.osmdroid.util.GeoPoint;
+import org.osmdroid.views.MapView;
+import org.osmdroid.views.overlay.Marker;
 
-public class MapFragment extends Fragment implements OnMapReadyCallback, ActivityCompat.OnRequestPermissionsResultCallback {
+public class MapFragment extends Fragment {
 
-    private GoogleMap mMap;
+    private MapView map = null;
     private FusedLocationProviderClient fusedLocationClient;
-    private static final int LOCATION_REQUEST_CODE = 101;
+    private LocationCallback locationCallback;
+    private static final int REQUEST_LOCATION_PERMISSION = 1;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_map, container, false);
+
+        Configuration.getInstance().setUserAgentValue("com.example.truckup");
+
+        map = (MapView) view.findViewById(R.id.map);
+        map.setMultiTouchControls(true);
+
         fusedLocationClient = LocationServices.getFusedLocationProviderClient(getActivity());
-        SupportMapFragment mapFragment = (SupportMapFragment) getChildFragmentManager().findFragmentById(R.id.map);
-        if (mapFragment != null) {
-            mapFragment.getMapAsync(this);
+
+        locationCallback = new LocationCallback() {
+            @Override
+            public void onLocationResult(LocationResult locationResult) {
+                if (locationResult == null) {
+                    return;
+                }
+                for (Location location : locationResult.getLocations()) {
+                    onLocationChanged(location);
+                }
+            }
+        };
+
+        if (ContextCompat.checkSelfPermission(getActivity(), Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            requestLocationPermission();
+        } else {
+            startLocationUpdates();
         }
-        // Initialize Places SDK
-        Places.initialize(getActivity().getApplicationContext(), "AIzaSyDt3Ux3-XjNzcyOfmDGVziom1u4UQ6ii2s");
+
         return view;
     }
 
-    @Override
-    public void onMapReady(GoogleMap googleMap) {
-        mMap = googleMap;
-        if (ActivityCompat.checkSelfPermission(getActivity(), Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED
-                && ActivityCompat.checkSelfPermission(getActivity(), Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-            ActivityCompat.requestPermissions(getActivity(), new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, LOCATION_REQUEST_CODE);
-            return;
-        }
-        mMap.setMyLocationEnabled(true);
-        getNearestServiceStations();
+    private void requestLocationPermission() {
+        ActivityCompat.requestPermissions(getActivity(), new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, REQUEST_LOCATION_PERMISSION);
     }
 
-    private void getNearestServiceStations() {
-        // Get the last known location
-        if (ActivityCompat.checkSelfPermission(getActivity(), Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(getActivity(), Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-            return;
+    private void startLocationUpdates() {
+        LocationRequest locationRequest = LocationRequest.create();
+        locationRequest.setInterval(60000);
+        locationRequest.setFastestInterval(30000);
+        locationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+
+        if (ContextCompat.checkSelfPermission(getActivity(), Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+            fusedLocationClient.requestLocationUpdates(locationRequest, locationCallback, null /* Looper */);
         }
-        fusedLocationClient.getLastLocation()
-                .addOnSuccessListener(getActivity(), location -> {
-                    if (location != null) {
-                        // Create a new Places client instance
-                        PlacesClient placesClient = Places.createClient(getActivity());
-
-                        // Define a Place Fields mask
-                        List<Place.Field> placeFields = Arrays.asList(Place.Field.NAME, Place.Field.ADDRESS, Place.Field.LAT_LNG);
-
-                        // Create a new request
-                        FindCurrentPlaceRequest request = FindCurrentPlaceRequest.newInstance(placeFields);
-
-                        // Call findCurrentPlace and handle the response
-                        Task<FindCurrentPlaceResponse> placeResponse = placesClient.findCurrentPlace(request);
-                        placeResponse.addOnCompleteListener(task -> {
-                            if (task.isSuccessful()) {
-                                FindCurrentPlaceResponse response = task.getResult();
-                                for (PlaceLikelihood placeLikelihood : Objects.requireNonNull(response).getPlaceLikelihoods()) {
-                                    Place place = placeLikelihood.getPlace();
-                                    if (place.getTypes().contains(Place.Type.GAS_STATION)) {
-                                        LatLng placeLatLng = place.getLatLng();
-                                        if (placeLatLng != null) {
-                                            Log.i("MapFragment", "Adding marker for " + place.getName());
-                                            mMap.addMarker(new MarkerOptions().position(placeLatLng).title(place.getName()));
-                                        }
-                                    }
-                                }
-                            } else {
-                                Exception exception = task.getException();
-                                if (exception != null) {
-                                    Log.e("MapFragment", "Error finding places", exception);
-                                }
-                            }
-                        });
-                    }
-                });
     }
 
     @Override
-    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
-        if (requestCode == LOCATION_REQUEST_CODE) {
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        if (requestCode == REQUEST_LOCATION_PERMISSION) {
             if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                if (ActivityCompat.checkSelfPermission(getActivity(), Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED
-                        && ActivityCompat.checkSelfPermission(getActivity(), Manifest.permission.ACCESS_COARSE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
-                    mMap.setMyLocationEnabled(true);
-                    getNearestServiceStations();
-                }
+                startLocationUpdates();
             } else {
-                Toast.makeText(getActivity(), "Location permission denied", Toast.LENGTH_SHORT).show();
+                Toast.makeText(getActivity(), "Permission denied", Toast.LENGTH_SHORT).show();
             }
         }
+    }
+
+    public void onLocationChanged(Location location) {
+        GeoPoint startPoint = new GeoPoint(location.getLatitude(), location.getLongitude());
+        map.getController().setCenter(startPoint);
+        map.getController().zoomTo(18.0);
+
+        Marker startMarker = new Marker(map);
+        startMarker.setPosition(startPoint);
+        startMarker.setAnchor(Marker.ANCHOR_CENTER, Marker.ANCHOR_BOTTOM);
+        startMarker.setTitle("My location");
+        startMarker.setIcon(getResources().getDrawable(R.drawable.location));
+        map.getOverlays().add(startMarker);
+
+        // After adding the marker, re-center the map to the marker's position and zoom in
+        map.getController().animateTo(startMarker.getPosition());
+        map.getController().zoomTo(18.0);
+        addServiceStationMarkers(location);
+    }
+    private void addServiceStationMarkers(Location location) {
+        double latitude = location.getLatitude();
+        double longitude = location.getLongitude();
+
+        // Overpass API query
+        String overpassQuery = "[out:json][timeout:25];" +
+                "(" +
+                "node[\"amenity\"=\"fuel\"](" +
+                (latitude - 0.1) + "," + (longitude - 0.1) + "," +
+                (latitude + 0.1) + "," + (longitude + 0.1) +
+                ");" +
+                ");" +
+                "out body;";
+
+        // URL for the Overpass API
+        String overpassUrl = "https://overpass-api.de/api/interpreter?data=" + Uri.encode(overpassQuery);
+
+        // Create a new request queue
+        RequestQueue requestQueue = Volley.newRequestQueue(getActivity());
+
+        // Create a new JSON request
+        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.GET, overpassUrl, null,
+                response -> {
+                    try {
+                        JSONArray elements = response.getJSONArray("elements");
+                        for (int i = 0; i < elements.length(); i++) {
+                            JSONObject element = elements.getJSONObject(i);
+                            JSONObject tags = element.getJSONObject("tags");
+                            String name = tags.has("name") ? tags.getString("name") : "Unnamed";
+                            double lat = element.getDouble("lat");
+                            double lon = element.getDouble("lon");
+
+                            // Add a marker for the service station
+                            GeoPoint point = new GeoPoint(lat, lon);
+                            Marker marker = new Marker(map);
+                            marker.setPosition(point);
+                            marker.setTitle(name);
+                            // Set the custom icon
+                            marker.setIcon(getResources().getDrawable(R.drawable.baseline_local_gas_station_24));
+
+                            map.getOverlays().add(marker);
+                        }
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                    }
+                },
+                error -> Log.e("Volley", error.toString())
+        );
+
+        // Add the request to the request queue
+        requestQueue.add(jsonObjectRequest);
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        map.onResume();
+        if (ContextCompat.checkSelfPermission(getActivity(), Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+            startLocationUpdates();
+        }
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        map.onPause();
+        fusedLocationClient.removeLocationUpdates(locationCallback);
     }
 }
