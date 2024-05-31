337541cffe0def0fd9db0803c1460357fb334b38
dan_jn <danjan2906@gmail.com>
2024-06-01 01:40:31 +0400

    guest mode added

--- app/src/main/java/com/example/truckup/Login.java
+++ app/src/main/java/com/example/truckup/Login.java
@@ -1,12 +1,8 @@
 package com.example.truckup;
 
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
-import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -14,9 +10,9 @@ import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import com.example.truckup.MainActivity;
-import com.example.truckup.R;
-import com.example.truckup.Register;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
@@ -26,7 +22,7 @@ import com.google.firebase.auth.FirebaseUser;
 public class Login extends AppCompatActivity {
 
     EditText editTextEmail, editTextPassword;
-    Button buttonLogin;
+    Button buttonLogin, guestLogin;
     FirebaseAuth mAuth;
     ProgressBar progressBar;
     TextView textView;
@@ -52,6 +48,7 @@ public class Login extends AppCompatActivity {
         buttonLogin = findViewById(R.id.sign_in);
         progressBar = findViewById(R.id.progressBar);
         textView = findViewById(R.id.sign_up_now);
+        guestLogin = findViewById(R.id.guest_login);
         textView.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -120,5 +117,36 @@ public class Login extends AppCompatActivity {
                 }
             }
         });
+
+        guestLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                progressBar.setVisibility(View.VISIBLE);
+                mAuth.signInWithEmailAndPassword("sictst1@gmail.com", "Samsung2023")
+                        .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                            @Override
+                            public void onComplete(@NonNull Task<AuthResult> task) {
+                                progressBar.setVisibility(View.GONE);
+                                if (task.isSuccessful()) {
+                                    FirebaseUser user = mAuth.getCurrentUser();
+                                    if (user != null && user.isEmailVerified()) {
+                                        Toast.makeText(getApplicationContext(), "Login successful", Toast.LENGTH_SHORT).show();
+                                        Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                                        startActivity(intent);
+                                        finish();
+                                    } else {
+                                        Toast.makeText(getApplicationContext(), "Please verify your email first", Toast.LENGTH_SHORT).show();
+                                    }
+                                } else {
+                                    Toast.makeText(Login.this, "Authentication failed.",
+                                            Toast.LENGTH_SHORT).show();
+                                }
+                            }
+                        });
+            }
+        });
+
+
+
     }
 }
